75387080c61b07f8816adf04e029d77f85e5fb3d
“Adria” <adriastephanie@hotmail.com>
2024-04-03 10:49:14 -0300

    Initial Commit

---
+++ AulaCG/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  AulaCG
+//
+//  Created by Adria Stephanie on 03/04/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
